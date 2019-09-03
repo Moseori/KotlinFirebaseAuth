bffc0ea1ee55717ce8068180eef8f31bb7d91032
moseori <haojile96@gmail.com>
2019-09-03 16:02:17 +0900

    googleloginING

--- app/src/main/java/com/example/Dicon/MainActivity.kt
+++ app/src/main/java/com/example/Dicon/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity() {
                     // If sign in fails, display a message to the user.
                     Log.w("fail", "signInWithCredential:failure", task.exception)
                 }
-
+//https://gigas-blog.tistory.com/76
                 // ...
             }
     }
